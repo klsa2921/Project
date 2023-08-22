45dc22c7c6ffd51cf8ee29501f20119919633ca5
klsa2921 <klsa.mallikanti2921@gmail.com>
2023-08-22 07:22:22 +0400

    added code

--- Frontend-master/src/app/login/login.component.ts
+++ Frontend-master/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  username: string;
+  password: string;
 
+  constructor(private http: HttpClient) {
+  }
+
+  login() {
+    const loginData = {
+      username: this.username,
+      password: this.password
+    };
+
+    this.http.post('http://localhost:2921/user/login', loginData)
+      .subscribe(
+        (response) => {
+          console.log('Login successful', response);
+        },
+        (error) => {
+          console.error('Login error', error);
+        }
+      );
+  }
 }
